813eea1e9c1a3a3d82b37ca950a8d9f4af24a3c2
phucvinh01 <nguyenphucvinh1920@gmail.com>
2024-02-25 17:29:25 +0700

    "Layout, Cleck and connect to MongoDB

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import MobieNav from '@/components/shared/MobieNav'
+import SideBar from '@/components/shared/SideBar'
+import React from 'react'
+
+const Layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <main className='root'>
+      <SideBar/>
+      <MobieNav/>
+        <div className='root-container'>
+            <div className="wrapper">
+                {children}
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Layout
